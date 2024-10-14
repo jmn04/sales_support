0b85800ce5ff4d06951be4fef8c4e42096a5ee77
kadowaki <jmn2012km@gmail.com>
2024-10-14 13:26:38 +0900

    不要な関数を削除

--- static/js/script.js
+++ static/js/script.js
@@ -2,10 +2,8 @@ function toggleAccordion(button) {
     const accordionItem = button.parentElement.querySelector('.accordion-item');
 
     if (button.textContent === '+') {
-        // accordionItem.style.display = 'block';
         button.textContent = '-';
     } else {
-        // accordionItem.style.display = 'none';
         button.textContent = '+';
     }
 }
@@ -17,35 +15,13 @@ function checkSelect(select) {
     // 企業が選択された場合、担当者セレクトボックスを有効化
     companySelect.disabled = select.value === "";
     if (select.value > 0) {
-        personSelect.disabled = false; // 有効化
+        personSelects.forEach(select => {
+            select.disabled = false; // 企業が選択された場合、すべての担当者を有効化
+        });
     } else {
-        // personSelect.selectedIndex = 0; // 選択をリセット
         personSelects.forEach(select => {
             select.value = 0;
             select.disabled = true; // 企業が選択されていない場合、すべての担当者を無効化
         });
     }
 }
-
-function selectNextItem(select, nextElemId) {
-    const personSelect = document.getElementById(nextElemId);
-    // 企業が選択された場合、担当者セレクトボックスを有効化
-    personSelect.disabled = select.value === "";
-    if (select.value > 0) {
-        personSelect.disabled = false; // 有効化
-    } else {
-        personSelect.disabled = true; // 無効化
-        personSelect.selectedIndex = 0; // 選択をリセット
-    }
-}
-
-// function toggleImportant() {
-//     const checkBox = document.getElementById('importantCheck');
-//     const elemBody = document.getElementById('body');
-//     // チェックボックスの状態に応じて背景色を変更
-//     if (checkBox.checked) {
-//         elemBody.style.background = '#ffff7f';
-//     } else {
-//         elemBody.style.background = '#fff';
-//     }
-// }
